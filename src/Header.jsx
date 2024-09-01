7404258c80028ad7e99cc49de351da4733cc8ee2
Neda Vaheb <n.vaheb.nv@gmail.com>
2024-09-01 05:07:09 -0700

    the Header.js and this css is complete

---
+++ src/Header.jsx
@@ -0,0 +1,31 @@
+import Form from "./Form.jsx";
+import styles from "./Header.module.css";
+
+function Header({ showForm, setShowForm, deleteHandler, allHandlere }) {
+  const addHandler = () => {
+    setShowForm(true);
+  };
+
+  return (
+    <>
+      <div className={styles.headerContainer}>
+        <h3>Contacts</h3>
+        <div>
+          <button className={styles.delete} onClick={deleteHandler}>
+            Delete
+          </button>
+          <button className={styles.add} onClick={allHandlere}>
+            All
+          </button>
+          <button className={styles.add} onClick={addHandler}>
+            {" "}
+            Add Contact
+          </button>
+          {showForm && <Form />}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
