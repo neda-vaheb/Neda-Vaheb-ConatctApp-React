ca5081dcd9119fb63b11a8171b249fa643c33bba
Neda Vaheb <n.vaheb.nv@gmail.com>
2024-09-01 05:16:22 -0700

    The App Component is compelete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,67 @@
-import "./App.css";
-import Search from "./Search.jsx";
+import { useState } from "react";
 import Header from "./Header.jsx";
+import Search from "./Search.jsx";
+import Form from "./Form.jsx";
+
+import ContactList from "./ContactList.jsx";
 
 function App() {
+  const [showForm, setShowForm] = useState(false);
+
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem("contacts")) || []
+  );
+  const [search, setSearch] = useState("");
+  const [check, setCheck] = useState(false);
+  const [checkContact, setCheckContact] = useState([]);
+
+  const deleteHandlerItem = (id) => {
+    const newContacts = contacts.filter((contact) => contact.id !== id);
+    setContacts(newContacts);
+  };
+
+  const deleteHandler = () => {
+    setContacts(checkContact);
+  };
+  const allHandlere = () => {
+    setContacts(JSON.parse(localStorage.getItem("contacts")));
+  };
+
   return (
     <>
-      <Search />
-      <Header />
+      <Search
+        search={search}
+        setSearch={setSearch}
+        contacts={contacts}
+        setContacts={setContacts}
+      />
+      <Header
+        check={check}
+        showForm={showForm}
+        setShowForm={setShowForm}
+        allHandlere={allHandlere}
+        deleteHandler={deleteHandler}
+      />
+      {showForm && (
+        <Form
+          contacts={contacts}
+          setContacts={setContacts}
+          setShowForm={setShowForm}
+        />
+      )}
+      {contacts.length ? (
+        <ContactList
+          contacts={contacts}
+          deleteHandlerItem={deleteHandlerItem}
+          check={check}
+          setCheck={setCheck}
+          setContacts={setContacts}
+          checkContact={checkContact}
+          setCheckContact={setCheckContact}
+        />
+      ) : (
+        <p className="alertText"> No Contacts</p>
+      )}
     </>
   );
 }
